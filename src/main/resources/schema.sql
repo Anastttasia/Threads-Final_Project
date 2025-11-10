9f35e9ea7897e73586593e8510ac5912deb0f3d4
Anastttasia <nastya.kaneva.99@gmail.com>
2025-11-11 00:25:50 +0300

    Fix DB 3

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products(
     product_url VARCHAR(300) PRIMARY KEY,
     product_description VARCHAR(1000),
-    product_price VARCHAR(300)
+    product_price VARCHAR(1000)
 )
